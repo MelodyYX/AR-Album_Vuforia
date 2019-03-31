b77ce791d610534a0465fab6dedbcca7047ad782
Melody <xyathere@sina.com>
2019-03-31 16:46:53 +0800

    3D Robot object and audio spectrum visualizaion added

---
+++ Assets/AudioVisualizationRes/Scripts/GetAudioSpectrumSamplesData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class GetAudioSpectrumSamplesData : MonoBehaviour {
+
+	AudioSource myAudioSource;
+	public static float[] mySpectrumSamples = new float[64];
+
+	// Use this for initialization
+	void Start () {
+		myAudioSource = GetComponent <AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetSpectrumAudioSource ();
+	}
+
+	void GetSpectrumAudioSource(){
+		myAudioSource.GetSpectrumData (mySpectrumSamples,0,FFTWindow.Blackman);
+	}
+}
+  
