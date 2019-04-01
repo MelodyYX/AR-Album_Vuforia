14c09b0dcf334d34847fa5df0d1db861dd3c0765
Melody <xyathere@sina.com>
2019-04-01 23:56:07 +0800

    Added a method to control spectrum cube's number

--- Assets/AudioVisualizationRes/Scripts/GetAudioSpectrumSamplesData.cs
+++ Assets/AudioVisualizationRes/Scripts/GetAudioSpectrumSamplesData.cs
@@ -7,7 +7,10 @@ using UnityEngine;
 public class GetAudioSpectrumSamplesData : MonoBehaviour {
 
 	AudioSource myAudioSource;
-	public static float[] mySpectrumSamples = new float[64];
+	public static int samplesNumber =128; // Must be a power of 2, min= 64,max= 8192.(ie.128/256)
+	public static int bandsNumber = 8; // Use this number when samplesNumber not in the range. Come with myFrequencyBands[] and GetFrequencyBands ()
+	public static float[] mySpectrumSamples = new float[samplesNumber];
+	public static float[] myFrequencyBands = new float[bandsNumber];
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +20,33 @@ public class GetAudioSpectrumSamplesData : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		GetSpectrumAudioSource ();
+		//GetFrequencyBands ();// Use this method when samples' number<64
 	}
 
 	void GetSpectrumAudioSource(){
 		myAudioSource.GetSpectrumData (mySpectrumSamples,0,FFTWindow.Blackman);
+	}
+
+	void GetFrequencyBands(){
+		
+		int count = 0;
+
+		for (int i=0; i < 8; i++){
+
+			float sum = 0;
+			int samplesNumber = (int)Mathf.Pow (2,i+1);
+
+			if (i == 7) {
+				samplesNumber += 2;
+			}
+
+			for (int j = 0; j < samplesNumber; j++) {
+				sum += mySpectrumSamples[count];
+				count++;
+			}
+
+			myFrequencyBands[i] = sum / samplesNumber;
+		}
 	}
 }
   
---
+++ Assets/AudioVisualizationRes/Scripts/InstantiateCubes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour {
+
+	public GameObject myCubePrefab;
+	GameObject[] sampleCubes = new GameObject[GetAudioSpectrumSamplesData.samplesNumber];
+	public int spectrumScale;
+
+	// Use this for initialization
+	void Start () {
+
+		// Create a circle of cubes
+		for(int i=0; i<GetAudioSpectrumSamplesData.samplesNumber; i++){
+			GameObject instanceOfCubePrefab = (GameObject)Instantiate (myCubePrefab);
+
+			instanceOfCubePrefab.transform.position = this.transform.position; ///Set this cube in the center of the object which it attach to.
+			//Debug.Log ("this transform: "+this.transform.gameObject.name);
+			instanceOfCubePrefab.transform.parent = this.transform;  ///Make this cube a child of the object which it attach to.
+
+			this.transform.eulerAngles = new Vector3 (0, -(360f/GetAudioSpectrumSamplesData.samplesNumber)*i, 0); /// The angle difference between each cube
+			instanceOfCubePrefab.transform.position = Vector3.forward*0.5f;  ///set a certain distance to center
+
+			instanceOfCubePrefab.name = "Sample Cube: " + i; // Change the name of every instance
+			sampleCubes [i] = instanceOfCubePrefab; // set this instance of cube into sampleCube list
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i=0; i<GetAudioSpectrumSamplesData.samplesNumber; i++){
+			if(sampleCubes != null){
+				sampleCubes [i].transform.localScale = new Vector3 (0.02f, (GetAudioSpectrumSamplesData.mySpectrumSamples[i] *(i+1)* spectrumScale) + 0.02f, 0.02f);
+				
+			}
+		}
+		
+	}
+}
