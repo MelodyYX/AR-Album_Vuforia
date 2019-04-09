c605493707d5a0e971f5fe8d43d9ca83b446cbf5
Melody <xyathere@sina.com>
2019-04-09 11:27:19 +0800

    Smoothed the jump of audio spectrums

--- Assets/AudioVisualizationRes/Scripts/GetAudioSpectrumSamplesData.cs
+++ Assets/AudioVisualizationRes/Scripts/GetAudioSpectrumSamplesData.cs
@@ -7,46 +7,131 @@ using UnityEngine;
 public class GetAudioSpectrumSamplesData : MonoBehaviour {
 
 	AudioSource myAudioSource;
-	public static int samplesNumber =128; // Must be a power of 2, min= 64,max= 8192.(ie.128/256)
-	public static int bandsNumber = 8; // Use this number when samplesNumber not in the range. Come with myFrequencyBands[] and GetFrequencyBands ()
+	public static int samplesNumber =64; // Must be a power of 2, min= 64,max= 8192.(ie.128/256)
 	public static float[] mySpectrumSamples = new float[samplesNumber];
+
+	float[] highestSpectrumValue = new float[samplesNumber];
+	public static float[] convertedSpectrumValue = new float[samplesNumber];
+
+	public static float[] myBuffer = new float[samplesNumber];
+	float[] bufferDecrease = new float[samplesNumber];
+
+	public static int bandsNumber = 8; // Use this number when samplesNumber lower the range. Work with myFrequencyBands[] and GetFrequencyBands ()
 	public static float[] myFrequencyBands = new float[bandsNumber];
+	float[] my512Samples = new float[512];
+
+
 
 	// Use this for initialization
 	void Start () {
-		myAudioSource = GetComponent <AudioSource>();
+		 myAudioSource = GetComponent <AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetSpectrumAudioSource ();
-		//GetFrequencyBands ();// Use this method when samples' number<64
-	}
+		 GetSpectrumAudioSource ();
+		 ConvertSpectrumValue ();  //Conver the spetrums value into a range between 0~1
+		 SmoothTheSpectrums ();  // Smoothing the spetrums 
+
+		//GetEightFrequencyBands (); //Using this function when bandsNumber=8.
+		//GetFrequencyBands(); //Using this function when bandsNumber<64 && power of 2.
+
+	} 
 
 	void GetSpectrumAudioSource(){
-		myAudioSource.GetSpectrumData (mySpectrumSamples,0,FFTWindow.Blackman);
+		 myAudioSource.GetSpectrumData (mySpectrumSamples,0,FFTWindow.Blackman);
 	}
 
-	void GetFrequencyBands(){
+	//Conver spetrum value into a range between 0~1
+	void ConvertSpectrumValue(){
+		for(int i=0; i<samplesNumber; i++){
+			if (mySpectrumSamples[i]> highestSpectrumValue[i]){
+				highestSpectrumValue [i] = mySpectrumSamples [i] ;
+			}
+		}
+
+		for (int i = 0; i < samplesNumber; i++) {
+			convertedSpectrumValue [i] = mySpectrumSamples [i] / highestSpectrumValue [i];
+		}
+	}
 		
-		int count = 0;
 
-		for (int i=0; i < 8; i++){
+	void SmoothTheSpectrums (){
+		
+		for (int i=0; i<samplesNumber; i++)
+		{
 
-			float sum = 0;
-			int samplesNumber = (int)Mathf.Pow (2,i+1);
+			if (mySpectrumSamples [i] >  myBuffer [i]) 
+			{
+				myBuffer [i] = mySpectrumSamples [i];
+				bufferDecrease [i] = 0.001f;
 
-			if (i == 7) {
-				samplesNumber += 2;
+			} 
+
+			if (mySpectrumSamples [i] < myBuffer [i]) 
+			{
+				if ((myBuffer [i] - bufferDecrease [i]) > 0) 
+				{
+					myBuffer [i] -= bufferDecrease [i];
+					mySpectrumSamples [i] = myBuffer [i];
+					bufferDecrease [i] *= 1.2f;
+
+				} else {
+					myBuffer [i] = 0;
+					mySpectrumSamples [i] = 0;
+					bufferDecrease [i] = 0.001f;
+				}
+
 			}
 
-			for (int j = 0; j < samplesNumber; j++) {
+		}
+		 
+	}
+		
+
+	//Using this function when bandsNumber=8.
+	void GetEightFrequencyBands(){  		
+
+		myAudioSource.GetSpectrumData (my512Samples,0,FFTWindow.Blackman);
+
+		int count = 0;
+
+		for (int i=0; i < 8; i++)
+		{
+
+			float sum = 0;
+			int summedSamplesNumber = (int)Mathf.Pow (2,i+1);
+			for (int j = 0; j < summedSamplesNumber; j++) 
+			{
 				sum += mySpectrumSamples[count];
 				count++;
 			}
 
-			myFrequencyBands[i] = sum / samplesNumber;
+			myFrequencyBands[i] = sum / summedSamplesNumber;
 		}
 	}
+
+
+	// Using this method when bandsNumber<64 && power of 2.       
+	void GetFrequencyBands(){  	
+
+		myAudioSource.GetSpectrumData (my512Samples,0,FFTWindow.Blackman);
+
+		int count = 0;
+
+		for (int i=0; i < bandsNumber; i++)
+		{
+
+			float sum = 0;
+			int summedSamplesNumber = (int)512/bandsNumber;
+			for (int j = 0; j < summedSamplesNumber; j++) 
+			{
+				sum += my512Samples[count];
+				count++;
+			}
+			myFrequencyBands[i] = sum / summedSamplesNumber;
+		}
+	}
+
 }
   
--- Assets/AudioVisualizationRes/Scripts/InstantiateCubes.cs
+++ Assets/AudioVisualizationRes/Scripts/InstantiateCubes.cs
@@ -13,28 +13,38 @@ public class InstantiateCubes : MonoBehaviour {
 
 		// Create a circle of cubes
 		for(int i=0; i<GetAudioSpectrumSamplesData.samplesNumber; i++){
+			
 			GameObject instanceOfCubePrefab = (GameObject)Instantiate (myCubePrefab);
 
 			instanceOfCubePrefab.transform.position = this.transform.position; ///Set this cube in the center of the object which it attach to.
-			//Debug.Log ("this transform: "+this.transform.gameObject.name);
 			instanceOfCubePrefab.transform.parent = this.transform;  ///Make this cube a child of the object which it attach to.
-
 			this.transform.eulerAngles = new Vector3 (0, -(360f/GetAudioSpectrumSamplesData.samplesNumber)*i, 0); /// The angle difference between each cube
-			instanceOfCubePrefab.transform.position = Vector3.forward*0.5f;  ///set a certain distance to center
+		    instanceOfCubePrefab.transform.position = Vector3.forward*0.5f;  ///set a certain distance to center
 
 			instanceOfCubePrefab.name = "Sample Cube: " + i; // Change the name of every instance
 			sampleCubes [i] = instanceOfCubePrefab; // set this instance of cube into sampleCube list
+			 
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		for(int i=0; i<GetAudioSpectrumSamplesData.samplesNumber; i++){
-			if(sampleCubes != null){
-				sampleCubes [i].transform.localScale = new Vector3 (0.02f, (GetAudioSpectrumSamplesData.mySpectrumSamples[i] *(i+1)* spectrumScale) + 0.02f, 0.02f);
-				
+			if(sampleCubes!=null){
+			   sampleCubes [i].transform.localScale = new Vector3 (0.05f, (GetAudioSpectrumSamplesData.mySpectrumSamples[i] *(i+0.01f) *spectrumScale) + 0.01f, 0.05f);
+			}
+
+		}
+			
+
+		/*
+		 * for(int i=0; i<GetAudioSpectrumSamplesData.bandsNumber; i++){
+			if(sampleCubes!=null){
+				sampleCubes [i].transform.localScale = new Vector3 (0.05f, (GetAudioSpectrumSamplesData.myFrequencyBands[i] *(i+0.01f) *spectrumScale) + 0.01f, 0.05f);
 			}
 		}
-		
+		*/
+
 	}
 }
